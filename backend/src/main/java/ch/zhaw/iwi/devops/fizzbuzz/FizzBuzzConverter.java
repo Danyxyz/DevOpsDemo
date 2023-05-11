7207015451805117f0ba4b41b829877583e1c6f4
Danyxyz <daniel-m@hotmail.ch>
2023-05-11 16:36:28 +0200

    Änderung der Return-Anweisunge

--- backend/src/main/java/ch/zhaw/iwi/devops/fizzbuzz/FizzBuzzConverter.java
+++ backend/src/main/java/ch/zhaw/iwi/devops/fizzbuzz/FizzBuzzConverter.java
@@ -1,10 +1,14 @@
 package ch.zhaw.iwi.devops.fizzbuzz;
 
+//Ich schreibe hier Kommentare
+// DevOps ist ein tolles Moduel
+// Sonarqube Testing 1 2 3 4...
+
 public class FizzBuzzConverter {
 
     public String convert(int i) {
         if (i % 3 == 0 && i % 7 == 0) {
-            return "FizzBuzz";
+            return "Im Changig the Code so i can see what happens in sonarqube";
         } else if (i % 3 == 0) {
             return "Fizz";
         } else if (i % 7 == 0) {
@@ -12,5 +16,6 @@ public class FizzBuzzConverter {
         }
         return String.valueOf(i);
     }
+
 
 }
