bee47e02799c0ba9555066305d1c9a4512cbdf4f
Danyxyz <daniel-m@hotmail.ch>
2023-05-15 11:58:18 +0200

    added assertions

--- backend/src/test/java/ch/zhaw/iwi/devops/calculator/CalculatorTest.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/calculator/CalculatorTest.java
@@ -4,62 +4,62 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 
-class CalculatorTest {
+public class CalculatorTest {
 
 	@Test
 	public void addPositiveNumbers() {
 		Calculator calculator = new Calculator();
 		int result = calculator.processNumbers(3,5,'+');
-		assertEquals(8, result);
+		Assertions.assertEquals(8, result);
 	}
 
 	@Test
 	public void subtractPositiveNumeber() {
 		Calculator calculator = new Calculator();
 		int result = calculator.processNumbers(8,3,'-');
-		assertEquals(5, result);
+		Assertions.assertEquals(5, result);
 	}
 
 	@Test
 	public void multiplayPositiveNumbers() {
-		Calculator calculator = new Calculator();
+		Calculator calculator= new Calculator();
 		int result = calculator.processNumbers(3, 5, '*');
-		assertEquals(15,result);
+		Assertions.assertEquals(15,result);
 	}
 
 	@Test
 	public void dividePositiveNumbers() {
 		Calculator calculator = new Calculator();
 		int result = calculator.processNumbers(10, 2, '/');
-		assertEquals(5, result);
+		Assertions.assertEquals(5, result);
 		}
 
 	@Test
 	public void moduloOperation() {
 		Calculator calculator = new Calculator();
 		int result = calculator.processNumbers(10, 3,'%');
-		assertEquals(1, result);
+		Assertions.assertEquals(1, result);
 	} 
 
 	@Test
 	public void maxPositiveInteger() {
 		Calculator calculator = new Calculator();
 		int result = calculator.processNumbers(Integer.MAX_VALUE, 1, '-');
-		assertEquals(Integer.MAX_VALUE - 1, result);
+		Assertions.assertEquals(Integer.MAX_VALUE - 1, result);
 	}
 
 	@Test
 	public void addOppositeNumbers() {
 		Calculator calculator = new Calculator();
 		int result = calculator.processNumbers(5, -5, '+');
-		assertEquals(0, result);
+		Assertions.assertEquals(0, result);
 	}
 
 	@Test
 	public void square() {
 		Calculator calculator = new Calculator();
 		int result = calculator.processNumbers(5, 0, 's');
-		assertEquals(25, result);
+		Assertions.assertEquals(25, result);
 
 	}
 
@@ -67,14 +67,14 @@ class CalculatorTest {
 	public void absoluteDifference() {
 		Calculator calculator = new Calculator();
 		int result = calculator.processNumbers(5, 10, 'a');
-		assertEquals(5, result);
+		Assertions.assertEquals(5, result);
 	}
 
 	@Test
 	public void maxNumber() {
 		Calculator calculator = new Calculator();
 		int result = calculator.processNumbers(5, 10, 'm');
-		assertEquals(10, result);
+		Assertions.assertEquals(10, result);
 	}
 }
 
